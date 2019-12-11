19cf96ce8cc907001d7d3e289a0d3d469e196543
SinLapis <stradust0001@gmail.com>
2019-12-11 21:33:33 +0800

    增加build

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -20,6 +20,26 @@ fun <T> async(x: suspend () -> T): Promise<T> {
     }
 }
 
+fun spider(url: String): String {
+    var content = ""
+    async {
+        val browser = Puppeteer.launch(object {}.also { it: dynamic ->
+            it.devtools = true
+            it.args = arrayOf("--no-sandbox", "--disable-setuid-sandbox")
+            it.headless = true
+        }).await()
+        try {
+            val page = browser.newPage().await()
+            page.goto(url, object {}.also { it: dynamic -> it.timeout = 10 * 1000 }).await()
+            page.waitFor(1000).await()
+            content = page.content().await() as String
+        } finally {
+            browser.close().await()
+        }
+    }
+    return content
+}
+
 fun main() {
     async {
         val browser = Puppeteer.launch(object {}.also { it: dynamic ->
