15ac39de63002bbed746669a5ae858adff1ddc67
SinLapis <stradust0001@gmail.com>
2019-12-11 17:29:50 +0800

    完成环境配置和爬虫代码

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,40 @@
+import kotlin.coroutines.*
+import kotlin.js.Promise
+
+suspend fun <T> Promise<T>.await(): T = suspendCoroutine { cont ->
+    then({ cont.resume(it) }, { cont.resumeWithException(it) })
+}
+
+fun <T> async(x: suspend () -> T): Promise<T> {
+    return Promise { resolve, reject ->
+        x.startCoroutine(object : Continuation<T> {
+            override val context = EmptyCoroutineContext
+
+            override fun resumeWith(result: Result<T>) {
+                if (result.isSuccess)
+                    resolve(result.getOrNull()!!)
+                else
+                    reject(result.exceptionOrNull()!!)
+            }
+        })
+    }
+}
+
+fun main() {
+    async {
+        val browser = Puppeteer.launch(object {}.also { it: dynamic ->
+            it.devtools = true
+            it.args = arrayOf("--no-sandbox", "--disable-setuid-sandbox")
+            it.headless = true
+        }).await()
+        try {
+            val page = browser.newPage().await()
+            page.goto("http://www.baidu.com", object {}.also { it: dynamic -> it.timeout = 10 * 1000 }).await()
+            page.waitFor(1000).await()
+            val content = page.content().await()
+            println(content.toString())
+        } finally {
+            browser.close().await()
+        }
+    }
+}
