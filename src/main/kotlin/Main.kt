4580ca198eb375e1b62351d86f32ae15e42a173e
SinLapis <stradust0001@gmail.com>
2019-12-13 14:13:34 +0800

    调试 无结果6

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,10 +16,8 @@ fun <T> async(x: suspend () -> T): Promise<T> {
             override fun resumeWith(result: Result<T>) {
                 if (result.isSuccess)
                     resolve(result.getOrNull()!!)
-                else {
-                    Log.warn(result.exceptionOrNull()?.toString() ?: "")
+                else
                     reject(result.exceptionOrNull()!!)
-                }
             }
         })
     }
@@ -50,48 +48,27 @@ object Main {
         Log.debug(param)
     }
 
-//    private fun spider(url: String): String {
-//        var content = ""
-//        async {
-//            val browser = Puppeteer.launch(object {}.also { it: dynamic ->
-//                it.devtools = true
-//                it.args = arrayOf("--no-sandbox", "--disable-setuid-sandbox")
-//                it.headless = true
-//                it.executablePath = "/usr/bin/chromium"
-//            }).await()
-//            try {
-//                val page = browser.newPage().await()
-//                page.goto(url, object {}.also { it: dynamic -> it.timeout = 10 * 1000 }).await()
-//                page.waitFor(1000).await()
-//                content = page.content().await() as String
-//            } finally {
-//                browser.close().await()
-//            }
-//        }
-//        return content
-//    }
-
     fun execute() {
         Log.info("spider start")
-        for (url in urls) {
-            async {
-                val browser = Puppeteer.launch(object {}.also { it: dynamic ->
-                    it.devtools = true
-                    it.args = arrayOf("--no-sandbox", "--disable-setuid-sandbox")
-                    it.headless = true
-                    it.executablePath = "/usr/bin/chromium"
-                }).await()
-                try {
+        async {
+            var content: String
+            val browser = Puppeteer.launch(object {}.also { it: dynamic ->
+                it.devtools = true
+                it.args = arrayOf("--no-sandbox", "--disable-setuid-sandbox")
+                it.headless = true
+                it.executablePath = "/usr/bin/chromium"
+            }).await()
+            try {
+                for (url in urls) {
                     val page = browser.newPage().await()
                     page.goto(url.url, object {}.also { it: dynamic -> it.timeout = 10 * 1000 }).await()
                     page.waitFor(1000).await()
-                    val content = page.content().await() as String
+                    content = page.content().await() as String
                     url.html = js("Buffer").from(content).toString("base64") as String
-                } catch (e: Exception) {
-                    Log.warn(e.toString())
-                } finally {
-                    browser.close().await()
+                    page.close().await()
                 }
+            } finally {
+                browser.close().await()
             }
         }
         Log.info("spider end")
